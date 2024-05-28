db89eeff597844ff1cf4a08e1219d05d3fdafb03
Nimarcs <moimonautremoi@gmail.com>
2024-05-28 15:52:02 +0200

    Ajout base de baignoire

--- src/main/java/fr/ul/miage/marcus/POOfxml/Baignoire.java
+++ src/main/java/fr/ul/miage/marcus/POOfxml/Baignoire.java
@@ -8,9 +8,46 @@ public class Baignoire {
     public static final Logger LOG = Logger.getLogger(Baignoire.class.getName());
 
 
+    public static final double DEFAULT = 100;
+    private double capacite;
+    private double volume;
 
     public Baignoire() {
-
+        this(DEFAULT);
     }
+
+    public Baignoire(double capacite){
+        this.capacite = capacite;
+    }
+
+    public boolean estPlein() {
+        return volume >= capacite;
+    }
+    public boolean estVide() {
+        return volume <= 0;
+    }
+
+    public double ajouterEau(double eau){
+        if (eau < 0.0) throw new IllegalArgumentException("L'eau est censé être positive");
+        volume += eau;
+        if (estPlein()){
+            volume = capacite;
+        }
+        return volume;
+    }
+
+    public double retirerEau(double eau){
+        if (eau < 0.0) throw new IllegalArgumentException("L'eau est censé être positive");
+        volume -= eau;
+        if (estVide()){
+            volume = 0.0;
+        }
+        return volume;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
 
 }
