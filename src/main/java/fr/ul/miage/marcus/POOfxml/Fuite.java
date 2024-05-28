3b4b3f312eec27c00fb6de1146abbbfd04dd5306
RICHIER Marcus <marcus.richier8@etu.univ-lorraine.fr>
2024-05-28 16:53:14 +0200

    Creation de la fuite et du robinet

---
+++ src/main/java/fr/ul/miage/marcus/POOfxml/Fuite.java
@@ -0,0 +1,92 @@
+package fr.ul.miage.marcus.POOfxml;
+
+import javafx.concurrent.ScheduledService;
+import javafx.concurrent.Task;
+
+import java.util.logging.Logger;
+
+public class Fuite extends ScheduledService<Baignoire> {
+
+    public static final Logger LOG = Logger.getLogger(Fuite.class.getName());
+
+    /**
+     * Debit par default de la fuite
+     */
+    private static final double DEFAUT_DEBIT = 10;
+
+
+    /**
+     * Baignoire associée
+     */
+    private final Baignoire baignoire;
+
+    /**
+     * Debit du robinet
+     */
+    private double debit;
+
+
+    /**
+     * Contructeur de fuite
+     * @param baignoire baignoire associée à la fuite
+     */
+    public Fuite(Baignoire baignoire){
+        super();
+        this.baignoire = baignoire;
+    }
+
+    /**
+     * Contrcuteur de fuite avec un debit precis
+     * @param baignoire baignoire associée à la fuite
+     * @param debit debit de la fuite
+     */
+    public Fuite(Baignoire baignoire, int debit){
+        this(baignoire);
+        if (checkDebit(debit))
+            this.debit = debit;
+        else
+            LOG.severe("La valeur de débit est interdite lors de la creation de la fuite, la valeur par défaut va donc être attribué");
+
+    }
+
+    /**
+     * Tâche lancer a chaque iteration
+     * @return Task tache à effectuer
+     */
+    @Override
+    protected Task<Baignoire> createTask() {
+        return new Task<>() {
+            @Override
+            protected Baignoire call() {
+                baignoire.retirerEau(debit);
+                return baignoire;
+            }
+        };
+    }
+
+    /**
+     * Getter du debit de la fuite
+     * @return debit de la fuite
+     */
+    public double getDebit() {
+        return debit;
+    }
+
+    /**
+     * Methode qui rebouche la fuite
+     */
+    public void boucher(){
+        this.debit = 0.0;
+    }
+
+    /**
+     * Methode qui verifie si la valeur du debit est valide
+     * Le débit doit être positif ou nul
+     * @param debit valeur du debit a verifier
+     * @return booleen à vrai si le débit est valide, faux sinon
+     */
+    private boolean checkDebit(double debit){
+        return debit >= 0.0;
+    }
+
+}
---
+++ src/main/java/fr/ul/miage/marcus/POOfxml/Robinet.java
@@ -0,0 +1,93 @@
+package fr.ul.miage.marcus.POOfxml;
+
+import javafx.concurrent.ScheduledService;
+import javafx.concurrent.Task;
+
+import java.util.logging.Logger;
+
+public class Robinet extends ScheduledService<Baignoire> {
+
+    public static final Logger LOG = Logger.getLogger(Robinet.class.getName());
+
+    /**
+     * Debit par default du robinet
+     */
+    private static final double DEFAUT_DEBIT = 10;
+
+    /**
+     * Baignoire associée a ce robinet
+     */
+    private final Baignoire baignoire;
+
+    /**
+     * Debit du robinet
+     */
+    private double debit;
+
+    /**
+     * Contructeur de robinet
+     * @param baignoire baignoire associée
+     */
+    public Robinet(Baignoire baignoire){
+        this.baignoire = baignoire;
+        this.debit = DEFAUT_DEBIT;
+    }
+
+    /**
+     * Constructeur de robinet avec un débit précis
+     * @param baignoire baignoire associée
+     * @param debit debit du robinet
+     */
+    public Robinet(Baignoire baignoire, int debit){
+        this(baignoire);
+        if (checkDebit(debit)){
+            this.debit = debit;
+        } else
+            LOG.severe("La valeur de débit est interdite lors de la creation du robinet, la valeur par défaut va donc être attribué");
+    }
+
+    /**
+     * Tâche lancer a chaque iteration
+     * @return Task tache à effectuer
+     */
+    @Override
+    protected Task<Baignoire> createTask() {
+        return new Task<>() {
+            @Override
+            protected Baignoire call() {
+                baignoire.ajouterEau(debit);
+                return baignoire;
+            }
+        };
+    }
+
+    /**
+     * Getter du débit
+     * @return debit du robinet
+     */
+    public double getDebit() {
+        return debit;
+    }
+
+    /**
+     * Setter du debit
+     * @param debit debit à définir
+     */
+    public void setDebit(double debit) {
+        if (checkDebit(debit))
+            this.debit = debit;
+        else
+            LOG.severe("Le debit doit être positif, changement ignoré");
+    }
+
+    /**
+     * Methode qui verifie si la valeur du debit est valide
+     * Le débit doit être positif ou nul
+     * @param debit valeur du debit a verifier
+     * @return booleen à vrai si le débit est valide, faux sinon
+     */
+    private boolean checkDebit(double debit){
+        return debit >= 0.0;
+    }
+
+}
