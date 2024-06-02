e860c85c765204f36b0c3012988cd49bf81ec13e
RICHIER Marcus <marcus.richier8@etu.univ-lorraine.fr>
2024-06-02 16:31:14 +0200

    Ajout javadoc Baignoire

--- src/main/java/fr/ul/miage/marcus/POOfxml/Baignoire.java
+++ src/main/java/fr/ul/miage/marcus/POOfxml/Baignoire.java
@@ -11,23 +11,52 @@ import java.util.logging.Logger;
 
 public class Baignoire {
 
-    //Creation d'un logger
+    /**
+     * Creation d'un logger
+     */
     public static final Logger LOG = Logger.getLogger(Baignoire.class.getName());
 
+    /**
+     * Capacité par defaut de la baignoire
+     */
+    public static final double CAPACITE_PAR_DEFAUT = 100;
+
+    /**
+     * Suite de valeurs servant à créer un graphique via JavaFX
+     */
     private XYChart.Series<Number, Number> xySeries;
+
+    /**
+     * Suite de valeurs servant à générer un csv de valeurs
+     */
     private List<String[]> listeExport;
 
+    /**
+     * Instant de départ de la simulation pour les tableaux
+     */
     private Instant top;
 
-
-    public static final double DEFAULT = 100;
+    /**
+     * Capacité de la baignoire
+     */
     private double capacite;
+
+    /**
+     * Volume occupé dans la baignoire
+     */
     private double volume;
 
+    /**
+     * Constructeur qui contruit une baignoire par défaut
+     */
     public Baignoire() {
-        this(DEFAULT);
+        this(CAPACITE_PAR_DEFAUT);
     }
 
+    /**
+     * Contructeur qui contruit une baignoire avec une capacité spécifique
+     * @param capacite capacité de la baignoire
+     */
     public Baignoire(double capacite){
         LOG.setLevel(App.currentLogLevel);
         this.capacite = capacite;
@@ -36,22 +65,14 @@ public class Baignoire {
         listeExport = new ArrayList<>();
     }
 
-    public boolean estPlein(double volume) {
-        return volume >= capacite;
-    }
-
-    public boolean estPlein() {
-        return estPlein(volume);
-    }
-
-    public boolean estVide(double volume) {
-        return volume <= 0;
-    }
-
-    public boolean estVide() {
-        return estVide(volume);
-    }
-
+    /**
+     * Méthode qui permet d'ajouter de l'eau dans la baignoire
+     *
+     * Cette méthode est synchronisée pour assurer que deux threads concurrents ne peuvent pas se chevaucher
+     *
+     * @param eau eau à ajouter
+     * @throws IllegalArgumentException Si l'eau fournie est négative
+     */
     public synchronized void ajouterEau(double eau){
         if (eau < 0.0) throw new IllegalArgumentException("L'eau est censé être positive");
         double nouveauVolume = volume + eau;
@@ -63,6 +84,14 @@ public class Baignoire {
         ajouterNouvelleValeurDansGraphiques();
     }
 
+    /**
+     * Méthode qui permet de retirer de l'eau dans la baignoire
+     *
+     * Cette méthode est synchronisée pour assurer que deux threads concurrents ne peuvent pas se chevaucher
+     *
+     * @param eau eau à retirer
+     * @throws IllegalArgumentException Si l'eau fournie est négative
+     */
     public synchronized void retirerEau(double eau){
         if (eau < 0.0) throw new IllegalArgumentException("L'eau est censé être positive");
         double nouveauVolume = volume - eau;
@@ -77,6 +106,45 @@ public class Baignoire {
 
     }
 
+    /**
+     * Permet de savoir si la baignoire serai pleine avec un volume théorique
+     * @param volume volume théorique
+     * @return vrai si la baignoire serait pleine, faux sinon
+     */
+    public boolean estPlein(double volume) {
+        return volume >= capacite;
+    }
+
+    /**
+     * Permet de savoir si la baignoire est pleine
+     * @return vrai si la baignoire est pleine, faux sinon
+     */
+    public boolean estPlein() {
+        return estPlein(volume);
+    }
+
+    /**
+     * Permet de savoir si la baignoire serai vide avec un volume théorique
+     * @param volume volume théorique
+     * @return vrai si la baignoire serait vide, faux sinon
+     */
+    public boolean estVide(double volume) {
+        return volume <= 0;
+    }
+
+    /**
+     * Permet de savoir si la baignoire est vide
+     * @return vrai si la baignoire est vide, faux sinon
+     */
+    public boolean estVide() {
+        return estVide(volume);
+    }
+
+    /**
+     * Setter de la capacité
+     * @param capacite capacité voulue
+     * @throws IllegalArgumentException est renvoyée si la capacité est négative
+     */
     public void setCapacite(double capacite) throws IllegalArgumentException {
         if (capacite < 0.0) {
             LOG.severe("Capacité d'une baignoire doit être positif, or " + capacite + " est négatif");
@@ -85,36 +153,63 @@ public class Baignoire {
         this.capacite = capacite;
     }
 
+    /**
+     * Getter du volume d'eau dans la baignoire
+     * @return volume d'eau dans la baignoire
+     */
     public double getVolume() {
         return volume;
     }
 
+    /**
+     * Capacité de la baignoire
+     * @return Capacité de la baignoire
+     */
     public double getCapacite() {
         return capacite;
     }
 
+    /**
+     * Vide complementement la baignoire
+     * Reinitialise les tableaux de valeurs pour une prochaine expérience
+     */
     public void vider() {
         volume = 0.0;
         xySeries.getData().clear();
         listeExport.clear();
     }
 
+    /**
+     * Définit le temps de début d'enregistrement des données de la baignoire
+     * @param top instant de début d'expérience
+     */
     public void setTop(Instant top) {
         this.top = top;
     }
 
+    /**
+     * Getter de la liste d'export
+     * @return liste d'export
+     */
     public List<String[]> getListeExport() {
         return Collections.unmodifiableList(listeExport);
     }
 
+    /**
+     * Getter du tableau de valeur à destination de JavaFx
+     * @return XYChart.Series de l'expérience courante
+     */
     public XYChart.Series<Number, Number> getXySeries() {
         return xySeries;
     }
 
-
+    /**
+     * Permet d'ajouter une nouvelle valeur dans les graphiques de l'expérience
+     */
     private void ajouterNouvelleValeurDansGraphiques() {
         java.time.Duration tempsDepuisDepart = java.time.Duration.between(top, Instant.now());
         listeExport.add(new String[]{String.valueOf(tempsDepuisDepart.toMillis()), String.valueOf(volume)});
+        //On ajoute la tache d'ajouter la valeur car cela ne tourne pas dans le même thread que javaFX
         Platform.runLater(
                 () -> xySeries.getData().add(new XYChart.Data<>(tempsDepuisDepart.toMillis(), volume))
         );
